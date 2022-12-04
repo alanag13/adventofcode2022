7f9ef0851ae9a37206eee887e1f20cb66c2bfedd
Alan Grgic <alan.grgic@gmail.com>
2022-12-03 19:22:07 -0600

    format past days

--- 1_calorie_counting/solution.py
+++ 1_calorie_counting/solution.py
@@ -3,28 +3,28 @@ input_file[-1] = "input.txt"
 
 from heapq import heappop, heappush
 
-with open("/".join(input_file)) as f:
+curr_sum = 0
+max_sum = 0
+max_heap = []
 
-    curr_sum = 0
-    max_sum = 0
-    max_heap = []
+with open("/".join(input_file)) as f:
     lines = f.readlines()
 
-    for i in range(len(lines)):
-        num = lines[i].strip()
+for i in range(len(lines)):
+    num = lines[i].strip()
 
-        if not num or i == len(lines) - 1:
-            heappush(max_heap, curr_sum)
+    if not num or i == len(lines) - 1:
+        heappush(max_heap, curr_sum)
 
-            if len(max_heap) > 3:
-                heappop(max_heap)
+        if len(max_heap) > 3:
+            heappop(max_heap)
 
-            max_sum = max(max_sum, curr_sum)
-            curr_sum = 0
-            
-            continue
+        max_sum = max(max_sum, curr_sum)
+        curr_sum = 0
+        
+        continue
 
-        curr_sum += int(num)
+    curr_sum += int(num)
 
-    print(f"Part one: {max_sum}")
-    print(f"Part two: {sum(max_heap)}")
+print(f"Part one: {max_sum}")
+print(f"Part two: {sum(max_heap)}")
--- 2_rock_paper_scissors/solution.py
+++ 2_rock_paper_scissors/solution.py
@@ -12,20 +12,21 @@ def get_result(theirs, mine):
 
 with open("/".join(input_file)) as f:
     records = f.readlines()
-    part_one_score = 0
-    part_two_score = 0
+    
+part_one_score = 0
+part_two_score = 0
 
-    for i in range(len(records)):
-        theirs, mine = records[i].split()
-        their_shape = (ord(theirs) - 65) + 1
-        part_one_my_shape = (ord(mine) - 23) - 65 + 1
-        part_two_my_shape = ((their_shape + part_one_my_shape) % 3) + 1
-        part_one_score += (
-            part_one_my_shape + get_result(their_shape, part_one_my_shape)
-        )
-        part_two_score += (
-            part_two_my_shape + get_result(their_shape, part_two_my_shape)
-        )
+for i in range(len(records)):
+    theirs, mine = records[i].split()
+    their_shape = (ord(theirs) - 65) + 1
+    part_one_my_shape = (ord(mine) - 23) - 65 + 1
+    part_two_my_shape = ((their_shape + part_one_my_shape) % 3) + 1
+    part_one_score += (
+        part_one_my_shape + get_result(their_shape, part_one_my_shape)
+    )
+    part_two_score += (
+        part_two_my_shape + get_result(their_shape, part_two_my_shape)
+    )
 
-    print(f"Part one: {part_one_score}")
-    print(f"Part two: {part_two_score}")
+print(f"Part one: {part_one_score}")
+print(f"Part two: {part_two_score}")
