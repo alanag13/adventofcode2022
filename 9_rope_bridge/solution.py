21e8e348255fb80be5b075491b93150a1d987ce1
Alan Grgic <alan.grgic@gmail.com>
2022-12-13 01:42:23 -0600

    day 9 pt 2

--- 9_rope_bridge/solution.py
+++ 9_rope_bridge/solution.py
@@ -1,47 +1,65 @@
 input_file = __file__.split("/")
 input_file[-1] = "input.txt"
 
-visited = [(0, 0)]
-
-head, prev_head = (0, 0), (0, 0)
 
 def move(knot, nav):
-    x, y = knot
-
     if nav == "R":
-        x += 1
+        knot[0] += 1
 
     elif nav == "L":
-        x -= 1
+        knot[0] -= 1
 
     elif nav == "U":
-        y += 1
+        knot[1] += 1
 
     elif nav == "D":
-        y -= 1
+        knot[1] -= 1
 
-    return x, y
+def get_tail_moves(head, tail):
+    moves = []
+    head_x, head_y = head
+    tail_x, tail_y = tail
+
+    x_dist, y_dist = head_x - tail_x, head_y - tail_y
+    max_dist = max(abs(x_dist), abs(y_dist))
+
+    if max_dist >= 2:
+        if x_dist > 0:
+            moves.append("R")
+        elif x_dist < 0:
+            moves.append("L")
+        
+        if y_dist > 0:
+            moves.append("U")
+        elif y_dist < 0:
+            moves.append("D")
+
+    return moves
 
 
 with open("/".join(input_file)) as f:
     lines = f.readlines()
 
-knots = [(0, 0)]
 
-for line in lines:
-    nav, dist = line.strip().split()
+def get_unique_tail_positions(rope_len):
+    visited = {(0, 0)}
+    knots = [[0, 0] for _ in range(rope_len)]
 
-    for idx in range(int(dist)):
-        orig_head = head
-        head = move(head, nav)
-        x, y = head
-        prev_x, prev_y = prev_head
+    for line in lines:
+        nav, dist = line.strip().split()
 
-        x_dist, y_dist = abs(prev_x - x), abs(prev_y - y)
-        total_dist = max(x_dist, y_dist)
+        for _ in range(int(dist)):
+            move(knots[0], nav)
 
-        if total_dist >= 2:
-            prev_head = orig_head
-            visited.append(prev_head)
+            for i in range(1, rope_len):
 
-print(len(set(visited)))
+                for tail_nav in get_tail_moves(knots[i-1], knots[i]):
+                    move(knots[i], tail_nav)
+
+            last_x, last_y = knots[-1]
+            visited.add((last_x, last_y))
+
+    return len(visited)
+
+print(f"Part one: {get_unique_tail_positions(2)}")
+print(f"Part two: {get_unique_tail_positions(10)}")
