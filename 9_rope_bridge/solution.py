5188e24739da2f1757085b3e76db418bc73f1374
Alan Grgic <alan.grgic@gmail.com>
2022-12-13 02:10:09 -0600

    day refactor

--- 9_rope_bridge/solution.py
+++ 9_rope_bridge/solution.py
@@ -2,7 +2,7 @@ input_file = __file__.split("/")
 input_file[-1] = "input.txt"
 
 
-def move(knot, nav):
+def move_head(knot, nav):
     if nav == "R":
         knot[0] += 1
 
@@ -15,26 +15,21 @@ def move(knot, nav):
     elif nav == "D":
         knot[1] -= 1
 
-def get_tail_moves(head, tail):
-    moves = []
-    head_x, head_y = head
-    tail_x, tail_y = tail
 
-    x_dist, y_dist = head_x - tail_x, head_y - tail_y
+def move_tail(head, tail):
+    x_dist, y_dist = head[0] - tail[0], head[1] - tail[1]
     max_dist = max(abs(x_dist), abs(y_dist))
 
     if max_dist >= 2:
         if x_dist > 0:
-            moves.append("R")
+            tail[0] += 1
         elif x_dist < 0:
-            moves.append("L")
+            tail[0] -= 1
         
         if y_dist > 0:
-            moves.append("U")
+            tail[1] += 1
         elif y_dist < 0:
-            moves.append("D")
-
-    return moves
+            tail[1] -= 1
 
 
 with open("/".join(input_file)) as f:
@@ -49,12 +44,10 @@ def get_unique_tail_positions(rope_len):
         nav, dist = line.strip().split()
 
         for _ in range(int(dist)):
-            move(knots[0], nav)
+            move_head(knots[0], nav)
 
             for i in range(1, rope_len):
-
-                for tail_nav in get_tail_moves(knots[i-1], knots[i]):
-                    move(knots[i], tail_nav)
+                move_tail(knots[i-1], knots[i])
 
             last_x, last_y = knots[-1]
             visited.add((last_x, last_y))
